a2e8110212c91af2545828963499a1365cdeb2bc
Leonardorossato <leonardorossato21@gmail.com>
2022-03-17 15:30:54 -0300

    rotas importadas falta testal-las com o insominia

--- routes/alunosRouter.js
+++ routes/alunosRouter.js
@@ -2,6 +2,11 @@ const express = require('express');
 const AlunosController = require('../controllers/alunosController');
 const router = express.Router()
 
+//Rota dos alunos
 router.get('/', AlunosController.getAllAlunos)
+router.get('/aluno:/id', AlunosController.getAlunosById)
+router.post('/aluno', AlunosController.createAluno)
+router.put('/aluno/:id', AlunosController.updatedAluno)
+router.delete('/aluno/:id', AlunosController.deletedAlunos)
 
 module.exports = router
