19cab97be9281a2d94be8d215692a07d583dbeee
Leonardorossato <leonardorossato21@gmail.com>
2022-03-17 15:13:48 -0300

    ajuste no import da conexaõ

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ const app = express();
 const bodyParser = require('body-parser');
 require('dotenv').config()
 const port = process.env.port
-const sqliteConnection = require('./connection/sqliteConnection')
+const sqliteConnection = require('./connection/sqlConnection')
+const alunosRouter = require('./routes/alunosRouter')
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.sqliteConnection = sqliteConnection
+
+app.use('/api/alunos', alunosRouter)
 
 app.listen(port, ()=>{
     console.log(`Server is running at port : ${port}`)
