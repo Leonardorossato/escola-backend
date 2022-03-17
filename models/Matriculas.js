b535ef1ebbc279d2a6f19f6836f5c5339ad7be7c
Leonardorossato <leonardorossato21@gmail.com>
2022-03-17 13:41:01 -0300

    model das matriculas ok

---
+++ models/Matriculas.js
@@ -0,0 +1,46 @@
+const sequelize = require('../connection/sqliteConnection')
+const {Model, DataTypes} = require('sequelize')
+
+class Matriculas extends Model {}
+
+Matriculas.init({
+    id: {
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+        type: DataTypes.INTEGER
+    },
+    status: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    alunosId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references:{
+            model: "Alunos",
+            key: "id"
+        }
+    },
+    turmasId:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: "Turmas",
+            key: "id"
+        }
+    }
+},{
+    sequelize,
+    modelName: "matriculas",
+    timestamps: true
+}, Matriculas.associate = (models)=>{
+    Matriculas.belongsTo(models.Alunos,{
+        foreignKey: 'alunosId'
+    })
+    Matriculas.belongsTo(models.Turmas, {
+        foreignKey: 'turmasId'
+    })
+})
+
+module.exports = Matriculas
