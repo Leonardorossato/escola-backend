ffba5169f326160e2229587f804de84faebe937f
Leonardorossato <leonardorossato21@gmail.com>
2022-03-17 15:14:06 -0300

    erro

--- connection/sqliteConnection.js
+++
@@ -1,8 +0,0 @@
-const sequelize = require('../config/sqlite')
-
-sequelize.sync().then(()=>{
-    console.log('Connection with sqlite was established successfully.')
-}).catch((err)=>{
-    console.log("Error connecting with sqlite: " + err)
-})
-
