ee5e546eba3a478c52e00bcb74673cc789b72b1c
Leonardorossato <leonardorossato21@gmail.com>
2022-03-18 11:25:07 -0300

    import da rota de turmas

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ require('dotenv').config()
 const port = process.env.port
 const sqliteConnection = require('./connection/sqlConnection')
 const alunosRouter = require('./routes/alunosRouter')
+const turmasRouter = require('./routes/turmasRouter')
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.sqliteConnection = sqliteConnection
 
 app.use('/api/alunos', alunosRouter)
+app.use('/api/turmas', turmasRouter)
 
 app.listen(port, ()=>{
     console.log(`Server is running at port : ${port}`)
