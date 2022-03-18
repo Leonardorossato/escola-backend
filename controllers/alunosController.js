13708c06d8873c3747df1beeb9cef6668def9370
Leonardorossato <leonardorossato21@gmail.com>
2022-03-18 11:25:30 -0300

    criação do metodo do controller de matricula ok

--- controllers/alunosController.js
+++ controllers/alunosController.js
@@ -1,4 +1,5 @@
 const Alunos = require('../models/Alunos')
+const Matriculas = require('../models/Matriculas')
 
 class AlunosController {
     static getAllAlunos = async(req, res) => {
@@ -45,6 +46,50 @@ class AlunosController {
         try {
             await Alunos.destroy({where : {id : Number(id)}})
             return res.status(200).json({message: `The aluno with id ${id} was deleted successfully.`})
+        } catch (error) {
+            return res.status(500).json({error: error.message})
+        }
+    }
+
+    static getMatriculaByAlunosIdAndTurmaId = async(req, res) =>{
+        const {alunosId, matriculasId} = req.params
+        try {
+            const matriculas = await Matriculas.findOne({where : {id : Number(alunosId, matriculasId)}})
+            return res.status(200).json(matriculas)
+        } catch (error) {
+            return res.status(500).json({error: error.message})
+        }
+    }
+    static createMatriculas = async(req, res) =>{
+        const {alunosId} = req.params
+        const newMatriculas = {...req.body, alunosId: Number(alunosId)}
+
+        try {
+            const matriculaCreated = await Matriculas.create(newMatriculas)
+            return res.status(201).json(matriculaCreated)
+        } catch (error) {
+            return res.status(500).json({error: error.message})
+        }
+    }
+    static updatedMatricula = async(req, res) => {
+        const {alunosId, matriculasId} = req.params
+        const updatedAluno = req.body
+
+        try {
+            await Matriculas.update(updatedAluno, {where: {id : Number(matriculasId), alunosId: Number(alunosId)}})
+            const matriculaUpdated = await Matriculas.findOne({where: {id : Number(matriculasId)}})
+            return res.status(200).json(matriculaUpdated)
+        } catch (error) {
+            return res.status(500).json({error: error.message})
+        }
+    }
+
+    static deletedMatricula = async(req, res) => {
+        const {matriculasId} = req.params
+
+        try {
+            await Matriculas.destroy({where: {id : Number(matriculasId)}})
+            return res.status(200).json({message: `id ${matriculasId} was successfully deleted`})
         } catch (error) {
             return res.status(500).json({error: error.message})
         }
